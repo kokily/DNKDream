d4756421a0fd135867776f473b946e0ac92a7fa3
kokily <kokily@naver.com>
2023-02-28 06:59:17 +0900

    _app config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,50 @@
 import type { AppProps } from 'next/app';
 import type { Session } from 'next-auth';
+import type { DefaultSeoProps } from 'next-seo';
+import { useState } from 'react';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
+import { DefaultSeo } from 'next-seo';
+import {
+  QueryClientProvider,
+  Hydrate,
+  dehydrate,
+  QueryClient,
+} from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { ToastContainer } from 'react-toastify';
 import GlobalStyle from '@/styles';
+import 'react-toastify/dist/ReactToastify.css';
+
+const title = 'D&K Dreams Blog - Welcome';
+
+const DefaultSEO: DefaultSeoProps = {
+  title,
+  canonical: 'https://dnkdream.com',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://dnkdream.com',
+    title,
+    siteName: 'D&K Dreams Blog',
+    images: [
+      {
+        url: '/assets/logo512.png',
+        width: 285,
+        height: 167,
+        alt: 'Image',
+      },
+    ],
+  },
+};
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+  const [queryClient] = useState(() => new QueryClient());
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <>
       <Head>
@@ -39,9 +76,18 @@ export default function App({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="/assets/logo192.png" />
       </Head>
-      <GlobalStyle />
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <DefaultSeo {...DefaultSEO} />
+        <GlobalStyle />
+
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+
+        <ToastContainer position="bottom-center" draggable={false} />
       </SessionProvider>
     </>
   );
