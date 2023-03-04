dbd6808dff4af376fca453833d0ed873f0a6e70c
kokily <kokily@naver.com>
2023-03-05 06:06:06 +0900

    Custom SSR server, Sync sitemap generator

---
+++ syncSitemap.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const fs = require('fs');
+const prettier = require('prettier');
+
+const SitemapGeneratedDate = new Date().toISOString();
+const API_DOMAIN = 'https://dnkdream.com/api';
+const FRONT_DOMAIN = 'https://dnkdream.com';
+const routes = ['/', '/about'];
+
+const formatting = (target) =>
+  prettier.format(target, {
+    parser: 'html',
+  });
+
+function makeSiteMapItem(url) {
+  f;
+  return `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${SitemapGeneratedDate}</lastmod>
+    </url>
+  `;
+}
+
+async function makePostSitemap() {
+  try {
+    const pages = routes.map((page) => FRONT_DOMAIN + page);
+    const pageSitemap = pages.map((page) => makeSiteMapItem(page)).join('');
+    let postsSitemap = '';
+    let counta = 0;
+
+    const posts = await axios.get(`${API_DOMAIN}/sitemap`);
+
+    if (!posts) {
+      console.log('Posts 가 존재하지 않습니다.');
+    }
+
+    postsSitemap = posts.data
+      .map((post) => {
+        counta += 1;
+        return makeSiteMapItem(`${FRONT_DOMAIN}/post/${post.id}`);
+      })
+      .join('');
+
+    console.log(`${counta} 개 포스트 사이트 맵 생성!`);
+
+    const generateIndexSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http:www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      ${pageSitemap}
+    </urlset>
+  `;
+
+    const generatePostSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http:www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      ${postsSitemap}
+    </urlset>
+  `;
+
+    const formattedIndexSitemap = formatting(generateIndexSitemap);
+    const formattedPostSitemap = formatting(generatePostSitemap);
+
+    fs.writeFileSync('./public/sitemap.xml', formattedIndexSitemap, 'utf8');
+    fs.writeFileSync('./public/post-sitemap.xml', formattedPostSitemap, 'utf8');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+makePostSitemap();
